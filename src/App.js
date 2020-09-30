77a914de4c59a8ff8de5356ee0301dc5ecc2b6e6
jdavellanedag <jdavellanedag@gmail.com>
2020-09-30 15:20:47 -0500

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import firebase from './utils/Firebase';
+import 'firebase/auth';
 
 function App() {
+
+  firebase.auth().onAuthStateChanged(currentuser => {
+    console.log(currentuser ? "Logged in" : "Not logged in");
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Electron/React App</h1>
     </div>
   );
 }
---
+++ src/utils/Firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAIwBLO7fo0vi3Naz9n1QK7H9vGmscxX04",
+    authDomain: "soundyapp-caed8.firebaseapp.com",
+    databaseURL: "https://soundyapp-caed8.firebaseio.com",
+    projectId: "soundyapp-caed8",
+    storageBucket: "soundyapp-caed8.appspot.com",
+    messagingSenderId: "533295517681",
+    appId: "1:533295517681:web:f6cb4ee1907aa5fa6c7e50"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
