5e34c456ecbd5604c33e1b3a0c57b5273b1474c2
jdavellanedag <juldave.gal@hotmail.com>
2021-03-18 23:18:39 -0500

    Node dependencies updated, register form added

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,9 +1,56 @@
 import React from "react";
+import {Button, Icon, Form, Input} from 'semantic-ui-react';
+import "firebase/auth";
+import firebase from "../../../utils/Firebase";
 
-const RegisterForm = () => {
+import "./RegisterForm.scss";
+
+const RegisterForm = ({setSelectedForm}) => {
+
+  const onSubmit = () => {
+    console.log("Formulario enviado");
+  }
+
   return (
-    <div>
-      <h1>REgisterForm</h1>
+    <div className="register-form">
+      <h1>Empieza a escuchar música con una cuenta gratis de Soundy</h1>
+      <Form onSubmit={onSubmit} autocomplete="off">
+        <Form.Field>
+          <Input
+            type="text"
+            name="email"
+            placeholder="Correo electronico"
+            icon="mail outline"
+            // onChange={}
+            // error={}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            icon="eye"
+            // onChange={}
+            // error={}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input
+            type="text"
+            name="username"
+            placeholder="¿Como deberíamos llamarte?"
+            icon="user circle outline"
+            // onChange={}
+            // error={}
+          />
+          <Button type="submit">Continuar</Button>
+        </Form.Field>
+      </Form>
+      <div className="register-form__options">
+        <p onClick={()=>setSelectedForm(null)}>Volver</p>
+        <p>¿Ya tienes Soundy? <span onClick={()=>setSelectedForm("login")}>Iniciar sesión</span></p>    
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import 'semantic-ui-css/semantic.min.css'
 import './index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
